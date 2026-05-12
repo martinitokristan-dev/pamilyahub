6a894ea3589ed60ba21d96dc52396b7b5838e688
Martinitokristan <kristan.martinito@urios.edu.ph>
2026-05-13 07:27:48 +0800

    Fix: Add SSL certificate to storage and update dynamic path resolution

--- backend/config/database.php
+++ backend/config/database.php
@@ -60,7 +60,7 @@ return [
             'strict' => true,
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA') ? env('MYSQL_ATTR_SSL_CA') : null,
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA') ? storage_path('app/' . env('MYSQL_ATTR_SSL_CA')) : null,
                 PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
             ]) : [],
         ],
