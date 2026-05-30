bd4a9eca687488cab0937f44b7ad2a3c90a1febb
Martinitokristan <kristan.martinito@urios.edu.ph>
2026-05-30 11:37:09 +0800

    update the version

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,9 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig(({ mode }) => {
   return {
+    build: {
+      chunkSizeWarningLimit: 1500,
+    },
     plugins: [
       vue(),
       VitePWA({
